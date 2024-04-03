bbb3fc44c4bbecee94368bd4fc0f9919452ce9eb
Samara Boudreault <2230912@etu.cchic.ca>
2024-04-03 11:02:13 -0400

    Début du programme/création dossiers etc

--- IntroductionASP/IntroductionASP/Program.cs
+++ IntroductionASP/IntroductionASP/Program.cs
@@ -26,6 +26,15 @@ namespace IntroductionASP
 
             app.UseAuthorization();
 
+            //À rajouter pour le coté Admin
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
+            });
+
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
