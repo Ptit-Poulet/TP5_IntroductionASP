56ccfeba791342726ddb0043d22f1de00b72d669
Sam <sambubulle@gmail.com>
2024-04-09 08:50:19 -0400

    start of admin area

---
+++ TP5_IntroASP/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP5_IntroASP.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TP5_IntroASP/Areas/Admin/Controllers/ReservationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TP5_IntroASP.Areas.Admin.Controllers
+{
+    public class ReservationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
